1c6dc5cc0375b1d8e1d9df9ea2b8195b006aa5a0
Daniel Correa <yo@danielgara.com>
2021-04-17 10:53:25 -0500

    imp: cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('Home', () => {
+
+  it('Home Page', () => {
+    cy.visit('http://localhost/deliveryapp/public/');
+    cy.contains('Home');
+  });
+
+  it('Login Page', () => {
+    cy.visit('http://localhost/deliveryapp/public/');
+    cy.get('a').contains('Login').click();
+    cy.url().should('include', '/login');
+    cy.get('label').should('contain', 'E-Mail Address');
+  });
+
+});
