6e86779bef2606da98d5d7fc3d557f53ccb2de4e
Daniel Correa <yo@danielgara.com>
2021-04-17 11:05:49 -0500

    fix: quick fix e2e

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,12 +1,12 @@
 describe('Home', () => {
 
   it('Home Page', () => {
-    cy.visit('http://localhost/deliveryapp/public/');
+    cy.visit('/');
     cy.contains('Home');
   });
 
   it('Login Page', () => {
-    cy.visit('http://localhost/deliveryapp/public/');
+    cy.visit('/');
     cy.get('a').contains('Login').click();
     cy.url().should('include', '/login');
     cy.get('label').should('contain', 'E-Mail Address');
