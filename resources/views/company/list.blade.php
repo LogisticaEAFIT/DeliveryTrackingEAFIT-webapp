189b10b27fad4703fb0cbee3f80f6f83b15ff157
Daniel Correa <yo@danielgara.com>
2021-05-19 12:26:03 -0500

    imp: changes in style

--- resources/views/company/list.blade.php
+++ resources/views/company/list.blade.php
@@ -4,20 +4,16 @@
 <div class="container-fluid padding-20">
     <div class="row">
         <div class="col-md-12">
-            <div class="row">
-                <div class="col-3">
-                    <div class="col-12">
-                        <form method="GET" action="{{ route('company.create') }}">
-                            <button type="submit" class="btn btn-success"><i class="fa fa-plus"></i> {{ __('company.input.create') }}</button>
-                        </form>
-                    </div>
+            <div class="row padding-bottom-20">
+                <div class="col-6">
+                    <form method="GET" action="{{ route('company.create') }}">
+                        <button type="submit" class="btn btn-success"><i class="fa fa-plus"></i> {{ __('company.input.create') }}</button>
+                    </form>
                 </div>
-                <div class="col-3"></div>
-                <div class="col-3"></div>                
-                <div class="col-3 align-right">
+                <div class="col-6 text-right">
                     <span class="badge rounded-pill bg-danger font-white pad-10">{{ __('company.red_info') }}</span>
                 </div>
-            </div><br/>
+            </div>
             <div class="card center-info">
                 <div class="card-header">{{ __('company.title_list') }}</div>
 
